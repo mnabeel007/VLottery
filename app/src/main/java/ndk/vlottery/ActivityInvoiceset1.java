b9842c39d2091fe53049b36560a5ed7914bf349a
MUHAMMED NABEEL <mnabeel007@gmail.com>
2018-08-19 01:31:08 +0530

    DESIGN SCREENS - 2.1

---
+++ app/src/main/java/ndk/vlottery/ActivityInvoiceset1.java
@@ -0,0 +1,13 @@
+package ndk.vlottery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityInvoiceset1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoiceset1);
+    }
+}
